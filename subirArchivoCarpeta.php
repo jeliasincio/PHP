3de3803b679fe9cf767385f000d6d9fd6e1fb72b
Edwin <ing.eliasincio123@gmail.com>
2023-04-02 17:42:26 -0500

    subir archivo a una carpeta creada

--- subirArchivoCarpeta.php
+++ subirArchivoCarpeta.php
@@ -14,7 +14,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Subir archivo en una carpeta creada</title>
+    <title>Subir archivo a una carpeta creada</title>
 </head>
 <body>
     <form action="#" enctype="multipart/form-data" method="post">
